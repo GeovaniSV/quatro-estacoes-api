94a9fb1d8e61131c6404e1017870b6ee98e91a83
Geovani Dos Santos Vargas <geovani.020204@gmail.com>
2025-09-11 13:28:42 -0400

    Cart and Items many to many relationship migration added

--- database/migrations/1756068907610_create_items_table.ts
+++ database/migrations/1756068907610_create_items_table.ts
@@ -6,7 +6,12 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('cart_id').unsigned().references('id').inTable('carts').onDelete('CASCADE')
+      table
+        .integer('product_id')
+        .unsigned()
+        .references('id')
+        .inTable('products')
+        .onDelete('CASCADE')
       table.integer('product_quantity')
       table.string('product_color', 80)
       table.decimal('item_price', 10, 2)
--- database/migrations/1757610959414_create_cart_itens_table.ts
+++ database/migrations/1757610959414_create_cart_itens_table.ts
@@ -1,14 +1,15 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'products'
+  protected tableName = 'cart_itens'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable().primary()
-      table.string('product_name', 80).notNullable()
-      table.string('product_description', 200)
-      table.decimal('product_price', 19.4)
+      table.increments('id')
+
+      table.integer('carts_id').unsigned().references('carts.id')
+      table.integer('items_id').unsigned().references('items.id')
+      table.unique(['carts_id', 'items_id'])
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
