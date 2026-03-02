26445ca4b4e1961a1701639b3dc2a66ec2e0120e
Geovani Dos Santos Vargas <geovani.020204@gmail.com>
2026-03-01 22:19:06 -0400

    fix: define __dirname for ESM in ace entrypoint

--- ace.js
+++ ace.js
@@ -17,6 +17,11 @@
 */
 
 import { Ignitor } from '@adonisjs/core'
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 new Ignitor(__dirname).ace()
 /**
